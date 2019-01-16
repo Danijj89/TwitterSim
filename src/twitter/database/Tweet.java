cffd0edb92cc2fd8b74ab96df2f8015212ea2bef
Danijj <zhan.d@husky.neu.edu>
2019-01-15 20:51:28 -0500

    completed the tweet builder and added a parsing iterator class

--- src/twitter/database/Tweet.java
+++ src/twitter/database/Tweet.java
@@ -11,7 +11,7 @@ public class Tweet {
   private final String datetime;
   private String message;
 
-  Tweet(long tweetId, int userId, String datetime, String message) throws IllegalArgumentException {
+  public Tweet(long tweetId, int userId, String datetime, String message) throws IllegalArgumentException {
     if (datetime == null || message == null) {
       throw new IllegalArgumentException("Given datetime or message is null");
     }
@@ -19,5 +19,41 @@ public class Tweet {
     this.userId = userId;
     this.datetime = datetime;
     this.message = message;
+  }
+
+  /**
+   * Gets the tweetId of this Tweet .
+   *
+   * @return the tweetId
+   */
+  public long getTweetId() {
+    return tweetId;
+  }
+
+  /**
+   * Gets the userId of this Tweet .
+   *
+   * @return the userId
+   */
+  public int getUserId() {
+    return userId;
+  }
+
+  /**
+   * Gets the datetime of this Tweet .
+   *
+   * @return the datetime
+   */
+  public String getDatetime() {
+    return datetime;
+  }
+
+  /**
+   * Gets the message of this Tweet .
+   *
+   * @return the message
+   */
+  public String getMessage() {
+    return message;
   }
 }
--- src/twitter/util/TwitterUtil.java
+++ src/twitter/util/TwitterUtil.java
@@ -1,66 +1,108 @@
 package twitter.util;
 
-import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Iterator;
 import java.util.Random;
+import twitter.database.Tweet;
 
 /**
  * Utility class that provides functionalities to build a tweet json file.
  */
 public class TwitterUtil {
-  private final Readable rd;
-  private final Appendable ap;
-
-  TwitterUtil
 
   /**
    * Builds a list of of tweets of a given size.
    * The userId of the tweet is randomly generated and the value
    * ranges from 1 to a given upper bound.
    * The datetime is generated randomly given a 'from' and a 'to' year (both inclusive).
-   * The message comes from parsing a given file and turned into an iterator.
-   * The result is saved into a given {@link Appendable}.
+   * The message comes from an implementation specific iterator.
+   * The result is written to a given filePath.
+   *
+   * NOTE: Years must be >= 1000.
    *
    * @param numTweets the number of tweets to be generated.
    * @param numUsers the range of user ids that will appear in the resulting list (from 0).
    * @param fromYear the lower bound year for the datetime range.
    * @param toYear the upper bound year for the datetime range.
-   * @param parser the parser to parse the given file path.
-   * @param fromFile the path to the file from which to read the messages.
-   * @param ap the appendable to append the result to.
+   * @param messages the {@link Iterator} from which to retrieve the messages from.
+   * @param toFilePath the path of the file to save the result to.
    */
   public void buildTweets(long numTweets, int numUsers, int fromYear, int toYear,
-      TwitterParser parser, String fromFile, Appendable ap) throws Exception {
-    JsonReader reader = null;
+      Iterator<String> messages, String toFilePath) {
+    if (numTweets < 1) {
+      throw new IllegalArgumentException("Given tweet number is negative or 0");
+    }
+    if (numUsers < 1) {
+      throw new IllegalArgumentException("Given number of users is negative or 0");
+    }
+    if (fromYear < 1000 || toYear < 1000) {
+      throw new IllegalArgumentException("Given years are negative");
+    }
+    if (fromYear > toYear) {
+      throw new IllegalArgumentException("Given from year is bigger than to year");
+    }
+    if (messages == null || toFilePath == null) {
+      throw new IllegalArgumentException("Given messages or toFilePath is null");
+    }
     JsonWriter writer = null;
     try {
-      writer = new JsonWriter(new FileWriter("tweets.json"));
+      writer = new JsonWriter(new FileWriter(toFilePath));
       writer.setIndent("    ");
-      reader = new JsonReader(new FileReader("reddit_jokes.json"));
       writer.beginArray();
-      reader.beginArray();
       Random userIdRandomizer = new Random();
       for (int i = 0; i < numTweets; i++) {
-        int userId = userIdRandomizer.nextInt(50000) + 1;
-        String datetime =
+        if (!messages.hasNext()) {
+          throw new IllegalStateException("Given messages are not enough!");
+        }
+        else {
+          int userId = userIdRandomizer.nextInt(numUsers) + 1;
+          String datetime = this.generateDT(fromYear, toYear);
+          String message = messages.next();
+          Tweet t = new Tweet(i + 1, userId, datetime, message);
+          this.writeMessage(writer, t);
+        }
       }
+      writer.endArray();
+      writer.close();
     } catch (IOException e) {
-
-    } finally {
-
+      e.printStackTrace();
     }
-    if (reader != null) {
-
-    }
-    reader.close();
-    writer.close();
   }
 
+  /**
+   * Generates a random mysql DATETIME.
+   *
+   * @param fromYear the year lower bound.
+   * @param toYear the year upper bound.
+   * @return a string formatted as a mysql DATETIME.
+   */
   public String generateDT(int fromYear, int toYear) {
-
+    Random r = new Random();
+    int year = r.nextInt(toYear - fromYear + 1) + fromYear;
+    int month = r.nextInt(12) + 1;
+    int day = r.nextInt(28) + 1;
+    int hour = r.nextInt(24);
+    int minute = r.nextInt(60);
+    int second = r.nextInt(60);
+    return String.format("%04d-%02d-%02d %02d:%02d:%02d",
+        year, month, day, hour, minute, second);
   }
 
+  /**
+   * Writes a given tweet to a given {@link JsonWriter}.
+   *
+   * @param writer the {@link JsonWriter}.
+   * @param tweet the tweet to be written.
+   * @throws IOException if the given writer is unable to write.
+   */
+  private void writeMessage(JsonWriter writer, Tweet tweet) throws IOException {
+    writer.beginObject();
+    writer.name("tweet_id").value(tweet.getTweetId());
+    writer.name("user_id").value(tweet.getUserId());
+    writer.name("datetime").value(tweet.getDatetime());
+    writer.name("message").value(tweet.getMessage());
+    writer.endObject();
+  }
 }
